08ad1e5580bb27042d7e2f298992ba7cdca21c92
brucewing <31296727+brucewing@users.noreply.github.com>
2017-09-06 14:09:04 +1000

    Iteration 2 source code added
    this is added by bruce

---
+++ Source code/app/src/main/java/com/zephyrs/android/onefriend/Test.java
@@ -0,0 +1,20 @@
+package com.zephyrs.android.onefriend;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by Barry on 3/9/17.
+ */
+
+public class Test extends AppCompatActivity {
+    Integer score;
+    Button back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+    }
+}
